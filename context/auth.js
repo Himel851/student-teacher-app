6e1092257c16aa919e75dd4e7b1707500b15ff80
Nazmul Himel <himel.cse96@gmail.com>
2023-07-03 11:23:38 +0600

    auth

--- context/auth.js
+++ context/auth.js
@@ -3,7 +3,27 @@ import { createContext, useContext, useEffect, useState } from "react";
 const AuthContext = createContext();
 
 const AuthProvider = ({ children }) => {
-  const [auth, setAuth] = useState([]);
+  const [auth, setAuth] = useState({
+    email: "",
+    token: "",
+    role: "",
+    user_type: "",
+    id: "",
+  });
+  useEffect(() => {
+    const data = localStorage.getItem("auth");
+    if (data) {
+      const parseData = JSON.parse(data);
+      setAuth({
+        ...auth,
+        email: parseData.email,
+        token: parseData.token,
+        role: parseData.role,
+        user_type: parseData.user_type,
+        id: parseData.id,
+      });
+    }
+  }, []);
  
   return (
     <AuthContext.Provider value={[auth, setAuth]}>
